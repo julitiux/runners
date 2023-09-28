843c50b6559e092083b02d4435903762dab9e3dd
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-28 02:49:57 -0600

    implement run method using Slf4j

--- src/main/java/com/runners/service/ApplicationRunnerService.java
+++ src/main/java/com/runners/service/ApplicationRunnerService.java
@@ -1,13 +1,15 @@
 package com.runners.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class ApplicationRunnerService implements ApplicationRunner {
   @Override
   public void run(ApplicationArguments args) throws Exception {
-
+    log.info("== Sending Message from ApplicationRunner ==");
   }
 }
