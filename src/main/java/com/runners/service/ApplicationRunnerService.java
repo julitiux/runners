dc0bed8682ac3b267f3f39097df60dcee7ad998b
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-09-28 02:42:08 -0600

    implement ApplicationRunner and add the run method

--- src/main/java/com/runners/service/ApplicationRunnerService.java
+++ src/main/java/com/runners/service/ApplicationRunnerService.java
@@ -1,4 +1,13 @@
 package com.runners.service;
 
-public class ApplicationRunnerService {
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationRunnerService implements ApplicationRunner {
+  @Override
+  public void run(ApplicationArguments args) throws Exception {
+
+  }
 }
